69f55e783a58d6f35a57b9b7e2532501bb1e90ac
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 08:41:25 +0300

    Add change display theme config component

---
+++ components/AppConfig/DisplayTheme.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DisplayTheme = () => {
+  return (
+    <div className="container flex flex-col gap-y-8">
+      <h2>Change Display Theme</h2>
+      <p>Coming soon 💯 🚀</p>
+    </div>
+  );
+};
+
+export default DisplayTheme;
