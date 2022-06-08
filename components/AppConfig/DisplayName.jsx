810d37c8220aa992899dd601c4c66bf6a88c9fcd
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 08:41:19 +0300

    Add change display name config component

---
+++ components/AppConfig/DisplayName.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DisplayName = () => {
+  return (
+    <div className="container flex flex-col gap-y-8">
+      <h2>Change Display Name</h2>
+      <p>Coming soon 💯 🚀</p>
+    </div>
+  );
+};
+
+export default DisplayName;
