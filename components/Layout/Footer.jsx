da57a3fbe38ae6229a629f69c57a2b51837182ed
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 21:02:19 +0300

    Update footer added gap to credits added easter egg

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Link from 'next/link';
 import Icon from '../Icon';
 import { useRouter } from 'next/router';
 
 const Footer = () => {
+  const [credits, setCredits] = useState('JOSA');
+  const [easter, setEaster] = useState(0);
   const [showIcon, setShowIcon] = useState(true);
   const router = useRouter();
 
@@ -11,23 +13,36 @@ const Footer = () => {
     router.pathname === '/settings' ? setShowIcon(false) : setShowIcon(true);
   }, [router]);
 
+  const roboLove = (e) => {
+    if (easter < 5) {
+      setEaster((easter += 1));
+    } else {
+      setCredits('@thamudi 💀');
+      setInterval(() => {
+        setCredits('JOSA');
+        setEaster(0);
+      }, 5000);
+    }
+  };
+
   return (
     <footer className="container flex justify-between py-2">
       {showIcon && (
-        <>
-          <Link href="/settings" passHref>
-            <div className="w-fit cursor-pointer">
-              <Icon icon="mdiCog" color="" />
-            </div>
-          </Link>
-          <div>
-            <span className="flex flex-row items-center">
-              <Icon icon="mdiCodeTags" /> with
-              <Icon icon="mdiRobotLove" /> by JOSA
-            </span>
+        <Link href="/settings" passHref>
+          <div className="w-fit cursor-pointer">
+            <Icon icon="mdiCog" color="" />
           </div>
-        </>
+        </Link>
       )}
+      <div>
+        <span className="flex flex-row items-center gap-x-2">
+          <Icon icon="mdiCodeTags" /> with
+          <div onClick={(e) => roboLove(e)}>
+            <Icon icon="mdiRobotLove" classes="cursor-pointer relative" />
+          </div>
+          by {credits}
+        </span>
+      </div>
     </footer>
   );
 };
