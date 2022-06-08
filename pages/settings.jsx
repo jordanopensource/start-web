73497e8badba72f7c8fea0914c6c6473e0520f35
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 08:41:59 +0300

    Updated settings page Added the settings components Added the navbar for settings

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import SettingsNav from '../components/Layout/SettingsNav';
+import DisplayName from '../components/AppConfig/DisplayName';
+import DisplayTheme from '../components/AppConfig/DisplayTheme';
 
 const Settings = () => {
+  const [active, setActive] = useState('display-name');
+
   return (
-    <div>
-      <h1 className="my-8">Settings</h1>
-      <Link href="/" passHref>
-        <p className="cursor-pointer underline">Go back</p>
-      </Link>
+    <div className="my-8 flex gap-x-16">
+      <div id="settings-nav">
+        <div className="mb-8">
+          <h2>Settings</h2>
+          <Link href="/" passHref>
+            <p className="cursor-pointer underline">Go back</p>
+          </Link>
+        </div>
+        <SettingsNav activeName={active} changeActive={setActive} />
+      </div>
+      <div id="display-config" className="mt-20">
+        {renderComponents(active)}
+      </div>
     </div>
   );
 };
 
 export default Settings;
+
+const renderComponents = (active) => {
+  switch (active) {
+    case 'display-name':
+      return <DisplayName />;
+    case 'display-theme':
+      return <DisplayTheme />;
+    default:
+      break;
+  }
+};
