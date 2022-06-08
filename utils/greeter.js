4805d2fa16843403db5ba0800de138bcae257929
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 11:55:53 +0300

    Add Name to greeter

--- utils/greeter.js
+++ utils/greeter.js
@@ -1,15 +1,16 @@
-export const greeter = ()=> {
+export const greeter = (displayName)=> {
   const now = new Date().getHours();
   let msg;
 
-  const greetingsSchemaRaw = 'Good evening!;Good afternoon!;Good morning!;Good night!';
+  const greetingsSchemaRaw = 'Good evening;Good afternoon;Good morning;Good night';
   const greetingsSchema = greetingsSchemaRaw.split(';');
+  const name = displayName? displayName: 'JOSAN';
 
   if (now >= 18) msg = greetingsSchema[0];
   else if (now >= 12) msg = greetingsSchema[1];
   else if (now >= 6) msg = greetingsSchema[2];
   else if (now >= 0) msg = greetingsSchema[3];
-  else msg = 'Hello!';
+  else msg = 'Hello';
 
-  return msg;
+  return `${msg} ${name}!`;
 };
