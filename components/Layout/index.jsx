ad497af75ec82008d15a0a2e64a4e44392b482b1
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 15:36:44 +0300

    General fixes Add scroller style

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -21,7 +21,7 @@ const Layout = (props) => {
         />
         <meta name="robots" content="index, follow" />
       </Head>
-      <main className="container my-20">{props.children}</main>
+      <main className="container my-20 h-screen">{props.children}</main>
       <Footer />
     </>
   );
