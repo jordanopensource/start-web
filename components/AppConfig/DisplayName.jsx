5db5c6049b142ec3300fa70bfb13294272807ed2
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 14:33:42 +0300

    Add display name settings

--- components/AppConfig/DisplayName.jsx
+++ components/AppConfig/DisplayName.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InputControl from '../Form/InputControl';
+import Button from '../Form/Button';
 
 const DisplayName = () => {
+  const [submitStatus, setSubmitStatus] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    try {
+      const displayName = localStorage.setItem(
+        'displayName',
+        e.target.name.value
+      );
+      actionHandler('Name updated successfully');
+    } catch (error) {
+      actionHandler('Error while saving the name');
+    }
+  };
+
+  const clearDisplayName = () => {
+    try {
+      const displayName = localStorage.removeItem('displayName');
+      actionHandler('Name Cleared successfully');
+    } catch (error) {
+      actionHandler('Error while saving the name');
+    }
+  };
+
+  const actionHandler = (msg) => {
+    setMessage(msg);
+    setSubmitStatus(!submitStatus);
+    setInterval(() => {
+      setSubmitStatus(!submitStatus);
+      setMessage('');
+    }, 5000);
+  };
   return (
     <div className="container flex flex-col gap-y-8">
       <h2>Change Display Name</h2>
-      <p>Coming soon 💯 🚀</p>
+      {submitStatus && <p>{message}</p>}
+      <div></div>
+      <form onSubmit={formHandler}>
+        <InputControl
+          name="Name"
+          type="text"
+          required={true}
+          placeholder="Your Name/ Alias"
+        />
+        <Button type="submit" value="save" />
+      </form>
+      <div>
+        <Button onClick={clearDisplayName} type="button" value="Clear Name" />
+      </div>
     </div>
   );
 };
