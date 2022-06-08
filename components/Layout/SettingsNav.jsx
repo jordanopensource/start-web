f61a2418b71b049681012373356f1fa3fa4a3e15
thamudi <tamim.hamoudi@gmail.com>
2022-06-08 08:40:44 +0300

    Add Settings nav

---
+++ components/Layout/SettingsNav.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const SettingsNav = (props) => {
+  const listItems = [
+    {
+      name: 'Display Name',
+      id: 'display-name',
+    },
+    {
+      name: 'Display Theme',
+      id: 'display-theme',
+    },
+  ];
+  return (
+    <nav>
+      <ul>
+        {listItems.map((item) => {
+          return (
+            <li
+              key={item.id}
+              id={item.id}
+              className={props.activeName === item.id ? 'active' : ''}
+              onClick={() => props.changeActive(item.id)}
+            >
+              {item.name}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default SettingsNav;
