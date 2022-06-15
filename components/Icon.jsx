37fc28bfef49e21a02fc4709e5acd593751fe441
thamudi <tamim.hamoudi@gmail.com>
2022-06-15 18:40:44 +0300

    Update icon component custom icon color based on selected theme

--- components/Icon.jsx
+++ components/Icon.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Icon as MDIcon } from '@mdi/react';
+import { useAppContext } from '../context/state';
 
 const Icon = (props) => {
   const MDIcons = require('@mdi/js');
-  let iconPath = MDIcons[props.icon];
-  if (!iconPath) {
-    iconPath = MDIcons.mdiCancel;
-  }
+  const myContext = useAppContext();
+  let iconPath = MDIcons[props.icon] ? MDIcons[props.icon] : MDIcons.mdiCancel;
+  let iconColor = myContext.currentTheme['--main-color']
+    ? myContext.currentTheme['--main-color']
+    : 'black';
 
   return (
     <MDIcon
       className={`icon ${props.classes}`}
       path={iconPath}
-      color={props.color ? props.color : 'black'}
+      color={props.color ? props.color : iconColor}
     />
   );
 };
