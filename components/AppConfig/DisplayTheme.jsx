34d8f9f84e192a468747ec973b9a83cbebeb757f
thamudi <tamim.hamoudi@gmail.com>
2022-06-15 18:41:26 +0300

    Update display theme now functional

--- components/AppConfig/DisplayTheme.jsx
+++ components/AppConfig/DisplayTheme.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useAppContext } from '../../context/state';
+import Theme from './Theme';
+import themeColors from '../../data/theme.json';
 
 const DisplayTheme = () => {
+  const myContext = useAppContext();
+  const themeNames = Object.keys(themeColors);
+  const changeBackgroundColor = (colorName) => {
+    myContext.changeColorTheme(colorName);
+  };
+
   return (
     <div className="container flex flex-col gap-y-8">
       <h2>Change Display Theme</h2>
-      <p>Coming soon 💯 🚀</p>
+      <div className="flex flex-wrap gap-x-16 gap-y-32">
+        {themeNames.map((theme, index) => {
+          return (
+            <div key={index}>
+              <Theme
+                colorObject={themeColors[theme]}
+                colorName={theme}
+                changeBackgroundColor={changeBackgroundColor}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
