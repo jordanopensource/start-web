53ad5e3118c1b32ea47162b0136d48461426b0b7
thamudi <tamim.hamoudi@gmail.com>
2022-06-16 10:33:33 +0300

    Refactor Component DisplayClock changed naming to dashboard

--- components/AppConfig/Dashboard.jsx
+++ components/AppConfig/Dashboard.jsx
@@ -6,7 +6,7 @@ function DisplayClock() {
   const myContext = useAppContext();
   return (
     <div className="container flex flex-col gap-y-8">
-      <h2>Change Clock Setting</h2>
+      <h2>Clock Setting</h2>
       <div className="flex flex-col items-center gap-y-8   lg:flex-row">
         <span className="mr-8">Toggle 24 hour mode</span>{' '}
         <Toggle
